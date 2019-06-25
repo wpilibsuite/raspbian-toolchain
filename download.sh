73a52f6fc2adce23e42dd5fefa3f045555020e60
Thad House <thadhouse1@gmail.com>
2019-06-24 18:23:58 -0700

    Fix downloads

--- download.sh
+++ download.sh
@@ -11,21 +11,21 @@ wget -nc -nv \
     http://isl.gforge.inria.fr/isl-${V_ISL}.tar.bz2 \
     https://ftp.gnu.org/gnu/gdb/gdb-${V_GDB}.tar.gz \
     https://sourceforge.net/projects/expat/files/expat/${Vw_EXPAT}/expat-${Vw_EXPAT}.tar.bz2 \
-    http://archive.raspbian.org/raspbian/pool/main/g/gcc-6/libgcc1_${Va_LIBGCC}_armhf.deb \
-    http://archive.raspbian.org/raspbian/pool/main/g/gcc-6/libgcc1-dbg_${Va_LIBGCC}_armhf.deb \
-    http://archive.raspbian.org/raspbian/pool/main/g/gcc-6/libgcc-6-dev_${Va_LIBGCC}_armhf.deb \
-    http://archive.raspbian.org/raspbian/pool/main/g/gcc-6/libatomic1_${Va_LIBSTDCPP}_armhf.deb \
-    http://archive.raspbian.org/raspbian/pool/main/g/gcc-6/libatomic1-dbg_${Va_LIBSTDCPP}_armhf.deb \
-    http://archive.raspbian.org/raspbian/pool/main/g/gcc-6/libstdc++6_${Va_LIBSTDCPP}_armhf.deb \
-    http://archive.raspbian.org/raspbian/pool/main/g/gcc-6/libstdc++6-6-dbg_${Va_LIBSTDCPP}_armhf.deb \
-    http://archive.raspbian.org/raspbian/pool/main/g/gcc-6/libstdc++-6-dev_${Va_LIBSTDCPP}_armhf.deb \
-    http://archive.raspbian.org/raspbian/pool/main/g/gcc-6/libasan3_${Va_LIBSTDCPP}_armhf.deb \
-    http://archive.raspbian.org/raspbian/pool/main/g/gcc-6/libasan3-dbg_${Va_LIBSTDCPP}_armhf.deb \
-    http://archive.raspbian.org/raspbian/pool/main/g/gcc-6/libgomp1_${Va_LIBSTDCPP}_armhf.deb \
-    http://archive.raspbian.org/raspbian/pool/main/g/gcc-6/libgomp1-dbg_${Va_LIBSTDCPP}_armhf.deb \
-    http://archive.raspbian.org/raspbian/pool/main/g/gcc-6/libubsan0_${Va_LIBSTDCPP}_armhf.deb \
-    http://archive.raspbian.org/raspbian/pool/main/g/gcc-6/libubsan0-dbg_${Va_LIBSTDCPP}_armhf.deb \
+    http://archive.raspbian.org/raspbian/pool/main/g/gcc-8/libgcc1_${Va_LIBGCC}_armhf.deb \
+    http://archive.raspbian.org/raspbian/pool/main/g/gcc-8/libgcc1-dbg_${Va_LIBGCC}_armhf.deb \
+    http://archive.raspbian.org/raspbian/pool/main/g/gcc-8/libgcc-8-dev_${Va_LIBGCC}_armhf.deb \
+    http://archive.raspbian.org/raspbian/pool/main/g/gcc-8/libatomic1_${Va_LIBSTDCPP}_armhf.deb \
+    http://archive.raspbian.org/raspbian/pool/main/g/gcc-8/libatomic1-dbg_${Va_LIBSTDCPP}_armhf.deb \
+    http://archive.raspbian.org/raspbian/pool/main/g/gcc-8/libstdc++6_${Va_LIBSTDCPP}_armhf.deb \
+    http://archive.raspbian.org/raspbian/pool/main/g/gcc-8/libstdc++6-8-dbg_${Va_LIBSTDCPP}_armhf.deb \
+    http://archive.raspbian.org/raspbian/pool/main/g/gcc-8/libstdc++-8-dev_${Va_LIBSTDCPP}_armhf.deb \
+    http://archive.raspbian.org/raspbian/pool/main/g/gcc-8/libasan5_${Va_LIBSTDCPP}_armhf.deb \
+    http://archive.raspbian.org/raspbian/pool/main/g/gcc-8/libasan5-dbg_${Va_LIBSTDCPP}_armhf.deb \
+    http://archive.raspbian.org/raspbian/pool/main/g/gcc-8/libgomp1_${Va_LIBSTDCPP}_armhf.deb \
+    http://archive.raspbian.org/raspbian/pool/main/g/gcc-8/libgomp1-dbg_${Va_LIBSTDCPP}_armhf.deb \
+    http://archive.raspbian.org/raspbian/pool/main/g/gcc-8/libubsan1_${Va_LIBSTDCPP}_armhf.deb \
+    http://archive.raspbian.org/raspbian/pool/main/g/gcc-8/libubsan1-dbg_${Va_LIBSTDCPP}_armhf.deb \
     http://archive.raspbian.org/raspbian/pool/main/g/glibc/libc6_${Va_LIBC}_armhf.deb \
     http://archive.raspbian.org/raspbian/pool/main/g/glibc/libc6-dbg_${Va_LIBC}_armhf.deb \
     http://archive.raspbian.org/raspbian/pool/main/g/glibc/libc6-dev_${Va_LIBC}_armhf.deb \
-    http://archive.raspbian.org/raspbian/pool/main/l/linux-4.9/linux-libc-dev_${Va_LINUX}_armhf.deb
+    http://archive.raspbian.org/raspbian/pool/main/l/linux/linux-libc-dev_${Va_LINUX}_armhf.deb
