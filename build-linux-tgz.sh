39ca63e5a70fcdd675b6403ca9d9cd2b027cd0a0
Peter Johnson <johnson.peter@gmail.com>
2020-11-11 20:05:28 -0800

    Add Linux i386 build

--- build-linux-tgz.sh
+++ build-linux-tgz.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-docker pull wpilib/toolchain-builder:14.04 \
- && docker run -v ${PWD}:/artifacts wpilib/toolchain-builder:14.04 bash -c "\
+docker pull wpilib/toolchain-builder:14.04$1 \
+ && docker run -v ${PWD}:/artifacts wpilib/toolchain-builder:14.04$1 bash -c "\
   cp /artifacts/download.sh /artifacts/repack.sh /artifacts/versions.sh . \
   && cp -R /artifacts/patches /artifacts/linux . \
   && echo 'downloading' && zsh download.sh \
